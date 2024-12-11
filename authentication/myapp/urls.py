81bab4170e7d32e1150e528ee0ba0a5048509b7d
t <ajeymalviya143@gmail.com>
2024-12-11 23:10:03 +0530

    added new changes into url file as adding new url for expect sso saml

--- authentication/myapp/urls.py
+++ authentication/myapp/urls.py
@@ -1,6 +1,7 @@
 # auth_app/urls.py
 from django.urls import path
-from .views import RegisterView, LoginView,  SamlLoginView, SamlACSView, ProtectedView
+from .views import RegisterView, LoginView,   ProtectedView
+# from .views import SamlLoginView, SamlACSView
 from rest_framework_simplejwt import views as jwt_views
 from myapp import views
 
@@ -9,9 +10,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('protected/', ProtectedView.as_view(), name='protected'),
-    path('sso/login/', SamlLoginView.as_view(), name='saml_login'),  # SAML login redirect
-    path('sso/acs/', SamlACSView.as_view(), name='saml_acs'),  # SAML Assertion Consumer Service (ACS) endpoint
-    path('protected/', ProtectedView.as_view(), name='protected'),
+    # path('sso/login/', SamlLoginView.as_view(), name='saml_login'),  # SAML login redirect
+    # path('sso/acs/', SamlACSView.as_view(), name='saml_acs'),  # SAML Assertion Consumer Service (ACS) endpoint
+    # path('protected/', ProtectedView.as_view(), name='protected'),
 
 ]
 
