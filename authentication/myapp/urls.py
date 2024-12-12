56f6fa2120627bcfb5f7890409fe037f3792c55a
t <ajeymalviya143@gmail.com>
2024-12-12 18:20:49 +0530

    added new url for admin authorised page protectedd view

--- authentication/myapp/urls.py
+++ authentication/myapp/urls.py
@@ -1,6 +1,6 @@
 # auth_app/urls.py
 from django.urls import path
-from .views import RegisterView, LoginView,   ProtectedView
+from .views import RegisterView, LoginView,   ProtectedView, ProtectedViewd
 # from .views import SamlLoginView, SamlACSView
 from rest_framework_simplejwt import views as jwt_views
 from myapp import views
@@ -12,7 +12,7 @@ urlpatterns = [
     path('protected/', ProtectedView.as_view(), name='protected'),
     # path('sso/login/', SamlLoginView.as_view(), name='saml_login'),  # SAML login redirect
     # path('sso/acs/', SamlACSView.as_view(), name='saml_acs'),  # SAML Assertion Consumer Service (ACS) endpoint
-    # path('protected/', ProtectedView.as_view(), name='protected'),
+    path('protectedd/', ProtectedViewd.as_view(), name='protectedd'),
 
 ]
 
