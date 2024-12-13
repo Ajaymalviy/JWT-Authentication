f3c66b70d0a2447b020692ccb678c3a2a996dacb
t <ajeymalviya143@gmail.com>
2024-12-13 21:38:40 +0530

    added new changes for url

--- authentication/myapp/urls.py
+++ authentication/myapp/urls.py
@@ -1,6 +1,6 @@
 # auth_app/urls.py
 from django.urls import path
-from .views import RegisterView, LoginView,   ProtectedView, ProtectedViewd
+from .views import RegisterView, LoginView,   ProtectedView, ProtectedViewd, ProtectedViewForAdminOrModerator
 # from .views import SamlLoginView, SamlACSView
 from rest_framework_simplejwt import views as jwt_views
 from myapp import views
@@ -13,6 +13,7 @@ urlpatterns = [
     # path('sso/login/', SamlLoginView.as_view(), name='saml_login'),  # SAML login redirect
     # path('sso/acs/', SamlACSView.as_view(), name='saml_acs'),  # SAML Assertion Consumer Service (ACS) endpoint
     path('protectedd/', ProtectedViewd.as_view(), name='protectedd'),
+    path('nothing/', ProtectedViewForAdminOrModerator.as_view(), name='nothing'),
 
 ]
 
