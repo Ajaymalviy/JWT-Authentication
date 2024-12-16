141713c80eb9e77d43ef8463e3b3f2d744910f9b
t <ajeymalviya143@gmail.com>
2024-12-16 15:49:57 +0530

    added new changes into model file as added one class and migrate it for session store for each user

--- authentication/myapp/models.py
+++ authentication/myapp/models.py
@@ -1,3 +1,8 @@
+# models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserSession(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    session_id = models.CharField(max_length=255, unique=True)
+    last_updated = models.DateTimeField(auto_now=True)
