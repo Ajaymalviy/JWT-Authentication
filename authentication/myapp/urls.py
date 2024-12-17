ea4921e3cb200e44589ed9e1347e9578163845df
t <ajeymalviya143@gmail.com>
2024-12-17 22:10:30 +0530

    added new changes into url as adding new url

--- authentication/myapp/urls.py
+++ authentication/myapp/urls.py
@@ -1,5 +1,6 @@
 # auth_app/urls.py
 from django.urls import path
+from .views import login_view
 from .views import RegisterView, LoginView,   ProtectedView, ProtectedViewd, ProtectedViewForAdminOrModerator
 # from .views import SamlLoginView, SamlACSView
 from rest_framework_simplejwt import views as jwt_views
@@ -7,7 +8,8 @@ from myapp import views
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('login/', login_view, name='login'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('protected/', ProtectedView.as_view(), name='protected'),
     # path('sso/login/', SamlLoginView.as_view(), name='saml_login'),  # SAML login redirect
