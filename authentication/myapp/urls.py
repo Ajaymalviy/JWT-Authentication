d68a7a146ba30781fa8d4592fdffc150ec61991c
Ajay Malviya <128165665+Ajaymalviy@users.noreply.github.com>
2025-03-02 23:14:40 +0530

    Update urls.py
    change url for saml

--- authentication/myapp/urls.py
+++ authentication/myapp/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('protected/', ProtectedView.as_view(), name='protected'),
-    # path('sso/login/', SamlLoginView.as_view(), name='saml_login'),  # SAML login redirect
-    # path('sso/acs/', SamlACSView.as_view(), name='saml_acs'),  # SAML Assertion Consumer Service (ACS) endpoint
+    path('sso/login/', SamlLoginView.as_view(), name='saml_login'),  # SAML login redirect
+    path('sso/acs/', SamlACSView.as_view(), name='saml_acs'),  # SAML Assertion Consumer Service (ACS) endpoint
     path('protectedd/', ProtectedViewd.as_view(), name='protectedd'),
     path('nothing/', ProtectedViewForAdminOrModerator.as_view(), name='nothing'),
 
